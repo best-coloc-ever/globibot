450009e669fce63b985d98a87619bbbf530bc334
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-02-06 20:48:43 +0100

    [Twitch] Added configurable emote sizes

--- app/bot/modules/twitch/emote_store.py
+++ app/bot/modules/twitch/emote_store.py
@@ -5,13 +5,21 @@ from imghdr import what as image_type
 
 from utils.logging import logger
 
+from collections import defaultdict
+
 import os
 
 class EmoteStore:
 
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
+
+    SIZES = [SMALL, MEDIUM, LARGE]
+
     def __init__(self):
-        self.url_store = {}
-        self.file_store = {}
+        self.url_store = defaultdict(dict)
+        self.file_store = defaultdict(dict)
 
         self.load_global()
         self.load_subscriber()
@@ -22,36 +30,41 @@ class EmoteStore:
     def load_global(self):
         response = requests.get('http://twitchemotes.com/api_cache/v2/global.json')
         data = response.json()
-        template = data['template']['medium']
         for emote_name, emote in data['emotes'].items():
             image_id = str(emote['image_id'])
-            self.url_store[emote_name] = template.replace('{image_id}', image_id)
+            for size in EmoteStore.SIZES:
+                template = data['template'][size]
+                self.url_store[emote_name][size] = template.replace('{image_id}', image_id)
 
     def load_subscriber(self):
         response = requests.get('http://twitchemotes.com/api_cache/v2/subscriber.json')
         data = response.json()
-        template = data['template']['medium']
         for channel in data['channels'].values():
             for emote in channel['emotes']:
                 code = emote['code']
                 image_id = str(emote['image_id'])
-                self.url_store[code] = template.replace('{image_id}', image_id)
+                for size in EmoteStore.SIZES:
+                    template = data['template'][size]
+                    self.url_store[code][size] = template.replace('{image_id}', image_id)
 
     def load_bttv(self):
+        BTTV_SIZES = ['1x', '2x', '3x']
         response = requests.get('https://api.betterttv.net/2/emotes')
         data = response.json()
         template = data['urlTemplate']
         for emote in data['emotes']:
-            self.url_store[emote['code']] = template.replace('{{id}}', emote['id']).replace('{{image}}', '2x')
+            for size, bttv_size in zip(EmoteStore.SIZES, BTTV_SIZES):
+                url = template.replace('{{id}}', emote['id']).replace('{{image}}', bttv_size)
+                self.url_store[emote['code']][size] = url
 
-    def get(self, emote_name):
+    def get(self, emote_name, size):
         # Already downloaded ?
-        file_name = self.file_store.get(emote_name)
+        file_name = self.file_store[emote_name].get(size)
         if file_name:
             return file_name
 
         # Does is exist ?
-        emote_url = self.url_store.get(emote_name)
+        emote_url = self.url_store[emote_name].get(size)
         if emote_url is None:
             return None
 
@@ -60,5 +73,5 @@ class EmoteStore:
         ext = image_type(temp_file)
         file_name = '{}.{}'.format(temp_file, ext)
         os.rename(temp_file, file_name)
-        self.file_store[emote_name] = file_name
+        self.file_store[emote_name][size] = file_name
         return file_name
--- app/bot/modules/twitch/module.py
+++ app/bot/modules/twitch/module.py
@@ -9,6 +9,7 @@ class Twitch(Module):
 
         self.emote_enabled_channels = set()
         self.emote_store = EmoteStore()
+        self.emote_size = EmoteStore.MEDIUM
 
     @command('!emote enable')
     async def enable_emotes(self, message):
@@ -24,9 +25,19 @@ class Twitch(Module):
         message = '`Twitch emotes` are now **disabled** in this channel'
         await self.respond(message)
 
+    @command('!emote size {size:w}')
+    async def change_emote_size(self, message, size):
+        size = size.lower()
+        if not size in EmoteStore.SIZES:
+            await self.respond('`{}` is not a valid size'.format(size))
+        else:
+            self.emote_size = size
+            await self.respond('Emotes are now `{}`'.format(size))
+
     @command('!{emote_name:w}')
     async def display_emote(self, message, emote_name):
         if message.channel in self.emote_enabled_channels:
-            emote_file = self.emote_store.get(emote_name)
+            emote_file = self.emote_store.get(emote_name, self.emote_size)
             if emote_file:
                 await self.respond_file(emote_file)
+
