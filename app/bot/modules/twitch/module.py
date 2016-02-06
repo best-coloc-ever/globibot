ae4015b3b61b735f7c1832c0f126fe108c5ddf38
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-02-06 21:01:03 +0100

    [Twitch] Now handling different emote sizes per channel

--- app/bot/modules/twitch/module.py
+++ app/bot/modules/twitch/module.py
@@ -2,6 +2,8 @@ from ..base import Module, command
 
 from .emote_store import EmoteStore
 
+from collections import defaultdict
+
 class Twitch(Module):
 
     def __init__(self, *args, **kwargs):
@@ -9,7 +11,7 @@ class Twitch(Module):
 
         self.emote_enabled_channels = set()
         self.emote_store = EmoteStore()
-        self.emote_size = EmoteStore.MEDIUM
+        self.emote_sizes = defaultdict(lambda: EmoteStore.MEDIUM)
 
     @command('!emote enable')
     async def enable_emotes(self, message):
@@ -27,17 +29,21 @@ class Twitch(Module):
 
     @command('!emote size {size:w}')
     async def change_emote_size(self, message, size):
+        channel = self.last_message.channel
         size = size.lower()
+
         if not size in EmoteStore.SIZES:
             await self.respond('`{}` is not a valid size'.format(size))
         else:
-            self.emote_size = size
+            self.emote_sizes[channel] = size
             await self.respond('Emotes are now `{}`'.format(size))
 
     @command('!{emote_name:w}')
     async def display_emote(self, message, emote_name):
+        channel = self.last_message.channel
+
         if message.channel in self.emote_enabled_channels:
-            emote_file = self.emote_store.get(emote_name, self.emote_size)
+            emote_file = self.emote_store.get(emote_name, self.emote_sizes[channel])
             if emote_file:
                 await self.respond_file(emote_file)
 
