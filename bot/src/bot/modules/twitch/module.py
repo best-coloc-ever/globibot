39ac333f3ec9a37d2ad27288feba2469c070d069
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-03-03 00:40:02 +0100

    Completely reworked the Twitch module Using proper parsing Added a lot of features

--- bot/src/bot/modules/twitch/module.py
+++ bot/src/bot/modules/twitch/module.py
@@ -1,13 +1,20 @@
 from bot.lib.module import Module
-from bot.lib.decorators import simple_command
+from bot.lib.decorators import command
 from bot.lib.helpers.hooks import master_only
+from bot.lib.helpers import parsing as p
 
 from .emote_store import EmoteStore
 from .kappa import Kappa
 
+from parse import parse
+
 from collections import defaultdict
 
+import random
+
 class Twitch(Module):
+
+    prefix = p.string('!emote')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,95 +30,54 @@ class Twitch(Module):
             len(self.emote_store.url_store))
         )
 
-    @simple_command('!emote Kappa on', master_only)
-    async def enable_kappa_mode(self, message):
-        self.kappa_mode = True
+    @command(prefix + p.bind(p.on_off_switch, 'enabled'), master_only)
+    async def enable_emotes(self, message, enabled):
+        if enabled:
+            self.emote_disabled_channels.discard(message.channel)
+        else:
+            self.emote_disabled_channels.add(message.channel)
 
         await self.send_message(
             message.channel,
-            'Kappa mode is `enabled` for emotes'
+            '`Twitch emotes` are now **{}** in this channel\n'
+                .format('enabled' if enabled else 'disabled')
         )
 
-    @simple_command('!emote Kappa off', master_only)
-    async def disable_kappa_mode(self, message):
-        self.kappa_mode = False
+    kappa_prefix = prefix + p.string('Kappa')
+
+    @command(kappa_prefix + p.bind(p.on_off_switch, 'enabled'), master_only)
+    async def enable_kappa_mode(self, message, enabled):
+        self.kappa_mode = enabled
 
         await self.send_message(
             message.channel,
-            'Kappa mode is `disabled` for emotes'
+            'Kappa mode is `{}` for emotes'
+                .format('enabled' if enabled else 'disabled')
         )
 
-    @simple_command('!emote Kappa level {level:f}')
+    kappa_level = p.int_range(0, 100)
+    @command(kappa_prefix + p.bind(kappa_level, 'level'), master_only)
     async def set_kappa_level(self, message, level):
-        if level >= 0. and level <= 1.0:
-            self.kappa.level = level
-
-            await self.send_message(
-                message.channel,
-                'Kappa mode is at `{}` %'.format(level * 100)
-            )
-
-    @simple_command('!emote enable', master_only)
-    async def enable_emotes(self, message):
-        self.debug(
-            'Enabling emotes in {}#{}'.format(message.server, message.channel)
-        )
-
-        self.emote_disabled_channels.discard(message.channel)
+        self.kappa.level = float(level) / 100
 
         await self.send_message(
             message.channel,
-            (
-                '`Twitch emotes` are now **enabled** in this channel\n'
-                'size is set to `{}`'
-            ).format(self.emote_sizes[message.channel])
+            'Kappa mode is at `{}` %'.format(level)
         )
 
-    @simple_command('!emote disable', master_only)
-    async def disable_emotes(self, message):
-        self.debug(
-            'Disabling emotes in {}#{}'.format(message.server, message.channel)
-        )
-
-        self.emote_disabled_channels.add(message.channel)
-
-        await self.send_message(
-            message.channel,
-            '`Twitch emotes` are now **disabled** in this channel'
-        )
-
-    @simple_command('!emote size {size:w}', master_only)
+    emote_size = p.one_of(p.string, *EmoteStore.SIZES) >> p.to_s
+    @command(prefix + p.bind(emote_size, 'size'), master_only)
     async def change_emote_size(self, message, size):
         channel = message.channel
         size = size.lower()
 
-        if not size in EmoteStore.SIZES:
-            await self.send_message(
-                channel,
-                '`{}` is not a valid size'.format(size)
-            )
-        else:
-            self.emote_sizes[channel] = size
-            await self.send_message(
-                channel,
-                'Emotes are now `{}`'.format(size)
-            )
+        self.emote_sizes[channel] = size
+        await self.send_message(
+            channel,
+            'Emotes are now `{}`'.format(size)
+        )
 
-    @simple_command('!{emote_name:S}')
-    @simple_command('!<:{emote_name:S}:{:d}>')
-    async def display_emote(self, message, emote_name):
-        channel = message.channel
-
-        if channel not in self.emote_disabled_channels:
-            size = self.emote_sizes[channel]
-            if self.kappa_mode:
-                await self.kappa.reply_to_emote(message, emote_name, size)
-            else:
-                emote_file = self.emote_store.get(emote_name, size)
-                if emote_file:
-                    await self.send_file(channel, emote_file)
-
-    @simple_command('!emote reload', master_only)
+    @command(prefix + p.string('reload'), master_only)
     async def reload_emotes(self, message):
         self.emote_store = EmoteStore()
         self.kappa.emote_store = self.emote_store
@@ -121,4 +87,61 @@ class Twitch(Module):
             'loaded {} twitch emotes'.format(
                 len(self.emote_store.url_store)
             )
+        )
+
+    def find_emote(self, word):
+        EMOTE_FORMAT = '<:{emote_name:S}:{:d}>'
+
+        if word.startswith('!'):
+            name = word[1:]
+            if name in self.emote_store.url_store.keys():
+                return name
+            parsed = parse(EMOTE_FORMAT, name)
+            if parsed:
+                return parsed.named['emote_name']
+
+    @command(p.bind(p.many(p.any_type), 'words'))
+    async def emotes(self, message, words):
+        emotes = [
+            emote for emote in [self.find_emote(word) for word in words]
+            if emote
+        ]
+
+        await self.display_emotes(message.channel, emotes)
+
+    async def display_emotes(self, channel, emotes):
+        if channel not in self.emote_disabled_channels and emotes:
+            size = self.emote_sizes[channel]
+            emote_file = self.emote_store.assemble(emotes, size)
+            if emote_file:
+                await self.send_file(channel, emote_file)
+
+    @command(
+        prefix + p.string('random') +
+        p.bind(p.maybe(p.int_range(1, 10)), 'count')
+    )
+    async def random_emote(self, message, count=1):
+        emote_names = list(self.emote_store.url_store.keys())
+        random.shuffle(emote_names)
+
+        await self.display_emotes(message.channel, emote_names[:count])
+        await self.send_message(
+            message.channel,
+            '({})'.format(', '.join(emote_names[:count]))
+        )
+
+    @command(prefix + p.string('theme') + p.bind(p.word, 'theme'))
+    async def themed(self, message, theme):
+        emote_names = list(self.emote_store.url_store.keys())
+        random.shuffle(emote_names)
+
+        themed_emote_names = [
+            emote_name for emote_name in emote_names
+            if theme.lower() in emote_name.lower()
+        ][:10]
+
+        await self.display_emotes(message.channel, themed_emote_names)
+        await self.send_message(
+            message.channel,
+            '({})'.format(', '.join(themed_emote_names))
         )
