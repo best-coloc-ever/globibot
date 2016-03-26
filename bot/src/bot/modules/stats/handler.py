29bc5d25dc4a3af004d56804241ec9d30d27b0de
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-03-26 08:29:51 +0100

    Added real time messaging capabilities with websockets

--- bot/src/bot/modules/stats/handler.py
+++ bot/src/bot/modules/stats/handler.py
@@ -1,12 +1,45 @@
 from tornado.web import RequestHandler
 from tornado.escape import json_encode
 
-class StatsHandler(RequestHandler):
+from discord import ChannelType
+
+class StatsServersHandler(RequestHandler):
 
     def initialize(self, module):
         self.module = module
 
-    async def get(self, server_id):
+    async def get(self, server_id=None):
         self.set_header('Content-Type', 'application/json')
-        messages = self.module.messages_per_server[server_id]
-        self.write(json_encode(messages))
+        if server_id is None:
+            await self.list_servers()
+        else:
+            await self.list_channels(server_id)
+
+    async def list_servers(self):
+        servers = [
+            {
+                'id': server.id,
+                'name': server.name,
+            }
+            for server in self.module.bot.enabled_servers
+        ]
+        self.write(json_encode(servers))
+
+    async def list_channels(self, server_id):
+        try:
+            server = next(
+                server for server in self.module.bot.enabled_servers
+                if server.id == server_id
+            )
+        except StopIteration:
+            self.set_status(404)
+        else:
+            channels = [
+                {
+                    'id': channel.id,
+                    'name': channel.name,
+                }
+                for channel in sorted(server.channels, key=lambda s: s.position)
+                if channel.type == ChannelType.text
+            ]
+            self.write(json_encode(channels))
--- bot/src/bot/modules/stats/module.py
+++ bot/src/bot/modules/stats/module.py
@@ -1,6 +1,7 @@
 from bot.lib.module import Module
 
-from .handler import StatsHandler
+from .handler import StatsServersHandler
+from .ws_handler import StatsWebSocketHandler
 
 from collections import defaultdict
 
@@ -9,17 +10,35 @@ class Stats(Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        context = {
+            'module': self
+        }
         self.bot.web.add_handlers(r'.*$', [
-            (r'/stats/([0-9]+)', StatsHandler, dict(module=self))
+            (r'/servers', StatsServersHandler, context),
+            (r'/servers/([0-9]+)', StatsServersHandler, context),
+            (r'/ws', StatsWebSocketHandler, context),
         ])
 
         self.messages_per_server = defaultdict(list)
+        self.ws_consumers = set()
 
-    def on_message(self, message):
-        self.messages_per_server[message.server.id].append({
+    async def on_message(self, message):
+        data = {
+            'server': {
+                'name': message.server.name,
+                'id': message.server.id,
+            },
+            'channel': {
+                'name': message.channel.name,
+                'id': message.channel.id,
+            },
             'author': {
                 'name': message.author.name,
                 'id': message.author.id,
             },
-            'content': message.content
-        })
+            'content': message.content,
+        }
+
+        self.messages_per_server[message.server.id].append(data)
+        for consumer in self.ws_consumers:
+            consumer.write_message(data)
