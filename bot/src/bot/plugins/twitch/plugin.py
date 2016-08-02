0dae1a4085b804baabd64aff8418420fdfeec39d
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-08-02 04:19:55 +0200

    Started a skeleton for a Twitch plugin

---
+++ bot/src/bot/plugins/twitch/plugin.py
@@ -0,0 +1,19 @@
+from bot.lib.plugin import Plugin
+from bot.lib.decorators import command
+
+from bot.lib.helpers import parsing as p
+from bot.lib.helpers import formatting as f
+from bot.lib.helpers.hooks import master_only
+
+class Twitch(Plugin):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @command(
+        p.string('!twitch') + p.string('status')
+                            + p.bind(p.word, 'channel'),
+        master_only
+    )
+    async def twitch_status(self, message, channel):
+        pass
