d89a5279b11c79ba46cd25c2d66952c2be6271f6
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-08-02 04:10:55 +0200

    Reworked the bot abstraction skeleton

--- bot/src/bot/constants.py
+++ bot/src/bot/constants.py
@@ -1,10 +1,6 @@
-GLOBIBOT_EMAIL_KEY = 'email'
-GLOBIBOT_PASSWORD_KEY = 'password'
-
-ENABLED_MODULES_KEY = 'modules'
-
+GLOBIBOT_TOKEN_KEY = 'token'
+ENABLED_PLUGINS_KEY = 'plugins'
 ENABLED_SERVERS_KEY = 'servers'
-
 MASTER_IDS_KEY = 'masters'
 DB_HOST_KEY = 'host'
 DB_USER_KEY = 'user'
