2ea3dee5a419537437adf31c55f6f467a6e22573
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-08-04 19:54:57 +0200

    [Twitter] now using polling because streaming don't work well I don't really know if it's the lib I'm using but it's just not reliable for now

--- bot/src/bot/plugins/twitter/plugin.py
+++ bot/src/bot/plugins/twitter/plugin.py
@@ -32,21 +32,26 @@ def format_tweet(tweet):
         screen_name = tweet['user']['screen_name'],
         text        = f.code_block(tweet['text']),
         retweets    = tweet['retweet_count'],
-        favourites  = tweet['user']['favourites_count']
+        favourites  = tweet['favorite_count']
     )
 
 class Twitter(Plugin):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+    def load(self):
         self.oauth = OAuth(
             self.config.get(c.ACCESS_TOKEN_KEY),
             self.config.get(c.ACCESS_TOKEN_SECRET_KEY),
             self.config.get(c.CONSUMER_KEY_KEY),
             self.config.get(c.CONSUMER_SECRET_KEY),
         )
+
         self.client = TwitterAPI(auth=self.oauth)
+
+        self.monitored = set()
+        self.restore_monitored()
+
+    def unload(self):
+        self.monitored.clear()
 
     '''
     Commands
@@ -60,7 +65,7 @@ class Twitter(Plugin):
     )
     async def last_tweet(self, message, screen_name):
         user = self.get_user(screen_name=screen_name)
-        tweets = self.get_tweets(user, count=1)
+        tweets = self.get_tweets(user['id'], count=1)
 
         if tweets:
             await self.send_message(
@@ -83,14 +88,35 @@ class Twitter(Plugin):
                 server_id = message.server.id
             ))
 
-            future = self.monitor_forever(user_id, message.server)
-            asyncio.ensure_future(future)
+            self.run_async(self.monitor_forever(user_id, message.server))
 
             await self.send_message(
                 message.channel,
                 'Now monitoring `{}` tweets'.format(screen_name),
                 delete_after=15
             )
+
+    @command(
+        twitter_prefix + p.string('unmonitor') + p.bind(p.word, 'screen_name'),
+        master_only
+    )
+    async def unmonitor(self, message, screen_name):
+        user = self.get_user(screen_name=screen_name)
+        user_id = user['id']
+
+        if user_id not in self.monitored:
+            return
+
+        with self.transaction() as trans:
+            trans.execute(q.remove_monitored, dict(
+                user_id   = user_id,
+                server_id = message.server.id
+            ))
+
+        await self.send_message(
+            message.channel,
+            'Stopped monitoring `{}`'.format(screen_name)
+        )
 
     @command(twitter_prefix + p.string('monitored'), master_only)
     async def monitored(self, message):
@@ -99,7 +125,7 @@ class Twitter(Plugin):
             monitored = [MonitoredChannel(*row) for row in trans.fetchall()]
 
             users = [
-                self.get_user(user_id=channel.user_id)['screen_name']
+                self.get_user_by_id(channel.user_id)['screen_name']
                 for channel in monitored
                 if str(channel.server_id) == message.server.id
             ]
@@ -109,20 +135,6 @@ class Twitter(Plugin):
                 'I\'m currently monitoring the following channels:\n{}'
                     .format(f.code_block(users))
             )
-
-    @command(twitter_prefix + p.string('restore'), master_only)
-    async def restore(self, message):
-        with self.transaction() as trans:
-            trans.execute(q.get_monitored)
-            monitored = [MonitoredChannel(*row) for row in trans.fetchall()]
-
-            for channel in monitored:
-                server = next(
-                    serv for serv in self.bot.servers
-                    if serv.id == str(channel.server_id)
-                )
-                future = self.monitor_forever(channel.user_id, server)
-                asyncio.ensure_future(future)
 
     '''
     Details
@@ -134,31 +146,52 @@ class Twitter(Plugin):
         except Exception:
             raise e.UserNotFound(screen_name)
 
-    def get_tweets(self, user, count):
+    def get_user_by_id(self, user_id):
+        return self.client.users.lookup(user_id=user_id)[0]
+
+    def get_tweets(self, user_id, count):
         try:
             return self.client.statuses.user_timeline(
-                user_id=user['id'],
-                count=count
+                user_id=user_id,
+                count=count,
+                exclude_replies=True
             )
         except:
             return None
 
+    def restore_monitored(self):
+        with self.transaction() as trans:
+            trans.execute(q.get_monitored)
+            monitored = [MonitoredChannel(*row) for row in trans.fetchall()]
+
+            for channel in monitored:
+                server = next(
+                    serv for serv in self.bot.servers
+                    if serv.id == str(channel.server_id)
+                )
+                self.run_async(self.monitor_forever(channel.user_id, server))
+
     async def monitor_forever(self, user_id, server):
-        while True:
-            self.debug('Looking for new tweets from {} now'.format(user_id))
+        self.monitored.add(user_id)
 
-            stream = TwitterStream(auth=self.oauth)
-            iterator = AsyncIterator()
-            iterator.start(stream.statuses.filter, follow=user_id)
+        tweets = self.get_tweets(user_id, count=1)
+        if not tweets:
+            return
+        last_tweet = tweets[0]
 
-            async for tweet in iterator:
-                self.debug(tweet)
-                if tweet and 'text' in tweet:
-                    if tweet['user']['id'] == user_id:
-                        await self.send_message(
-                            server.default_channel,
-                            format_tweet(tweet)
-                        )
+        self.debug('Monitoring new tweets from {}'.format(user_id))
 
-            self.debug('Hangup, reconnecting in 15...')
-            await asyncio.sleep(15)
+        while user_id in self.monitored:
+            await asyncio.sleep(45)
+
+            tweets = self.get_tweets(user_id, count=1)
+            if tweets:
+                latest = tweets[0]
+                if latest['id'] != last_tweet['id']:
+                    last_tweet = latest
+                    await self.send_message(
+                        server.default_channel,
+                        format_tweet(latest)
+                    )
+
+        self.debug('No longer monitoring {}'.format(user_id))
--- bot/src/bot/plugins/twitter/queries.py
+++ bot/src/bot/plugins/twitter/queries.py
@@ -7,3 +7,9 @@ add_monitored = '''
     insert into twitter_monitored_channel (user_id, server_id)
     values                                (%(user_id)s, %(server_id)s)
 '''
+
+remove_monitored = '''
+    delete from twitter_monitored_channel
+        where   user_id = %(user_id)s
+            and server_id = %(server_id)s
+'''
