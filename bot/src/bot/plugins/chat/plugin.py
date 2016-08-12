21b9501e9e06bc5add4c0f3305b2200b625716f9
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-08-12 15:45:27 +0200

    [Chat] allowed everyone to have a personal bot to talk to in DM

--- bot/src/bot/plugins/chat/plugin.py
+++ bot/src/bot/plugins/chat/plugin.py
@@ -6,10 +6,14 @@ from bot.lib.helpers.hooks import master_only
 
 from cleverbot import Cleverbot
 
+from collections import defaultdict
+
 class ChatBot(Plugin):
 
     def load(self):
         self.chat_bot = Cleverbot()
+
+        self.dm_bots = defaultdict(Cleverbot)
 
     @command(
         p.bind(p.mention, 'user') + p.bind((p.many(p.any_type >> p.to_s)), 'words'),
@@ -21,6 +25,21 @@ class ChatBot(Plugin):
 
         await self.bot.send_typing(message.channel)
         response = self.chat_bot.ask(' '.join(words))
+
+        if response:
+            await self.send_message(
+                message.channel,
+                response
+            )
+
+    @command(p.bind((p.many(p.any_type >> p.to_s)), 'words'))
+    async def answer_dm(self, message, words):
+        if message.server:
+            return
+
+        await self.bot.send_typing(message.channel)
+        bot = self.dm_bots[message.author.id]
+        response = bot.ask(' '.join(words))
 
         if response:
             await self.send_message(
