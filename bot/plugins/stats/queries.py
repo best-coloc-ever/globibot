5396c2997d73b13d938abd162309f92c35b9ffd9
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-10-31 05:22:11 +0100

    [Stats] Simplified the top games query Fetching the associated top user was too greedy performance wise the way it was written

--- bot/plugins/stats/queries.py
+++ bot/plugins/stats/queries.py
@@ -1,32 +1,24 @@
 add_game_times = lambda count: '''
-    insert into game_played_time (author_id, name, duration)
-    values                       {}
-    on conflict                  (author_id, name)
-    do update set    duration = game_played_time.duration + EXCLUDED.duration
-              where  game_played_time.author_id = EXCLUDED.author_id
-                and  game_played_time.name = EXCLUDED.name
+    insert into     game_played_time (author_id, name, duration)
+    values          {}
+    on conflict     (author_id, name)
+    do update set   duration = game_played_time.duration + EXCLUDED.duration
+              where game_played_time.author_id = EXCLUDED.author_id
+                and game_played_time.name = EXCLUDED.name
 '''.format(','.join(['%s'] * count))
 
 author_games = '''
-    select id, author_id, name, duration, created_at from game_played_time
-    where  author_id = %(author_id)s
-    order by duration desc
+    select          id, author_id, name, duration, created_at
+        from        game_played_time
+        where       author_id = %(author_id)s
+        order by    duration desc
 '''
 
 top_games = '''
-    select  g.name,
-        sum(g.duration),
-        count(g.author_id),
-        (select     author_id
-         from       game_played_time
-         where      name = g.name
-            and     author_id in %(authors_id)s
-         order by   duration desc
-         limit      1)
-
-    from        game_played_time as g
-    where       author_id in %(authors_id)s
-    group by    g.name
-    order by    sum(g.duration) desc
-    limit       %(limit)s
+    select          name, sum(duration), count(author_id)
+        from        game_played_time
+        where       author_id in %(authors_id)s
+        group by    name
+        order by    sum(duration) desc
+        limit       %(limit)s
 '''
