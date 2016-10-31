4748bbab037870931146cd55aeb512f1b87e503a
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-10-31 04:18:10 +0100

    [Logger] Fixed a bug where getting the start of next day would fail

--- bot/plugins/logger/handler.py
+++ bot/plugins/logger/handler.py
@@ -3,7 +3,7 @@ from globibot.lib.web.decorators import authenticated, respond_json
 
 from . import queries as q
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import time
 
 def user_data(user_snowflake, server):
@@ -83,11 +83,8 @@ class LogsApiTopHandler(SessionHandler):
             ))
 
             activity = []
-            next_day = datetime.now()
-            next_day.replace(
-                hour=0, minute=0, second=0, microsecond=0,
-                day=next_day.day + 1
-            )
+            next_day = datetime.now() + timedelta(days=1)
+            next_day.replace(hour=0, minute=0, second=0, microsecond=0)
 
             for action_count, unique_message_count, deleted_count, start in trans.fetchall():
                 days_from = int((next_day.timestamp() - start.timestamp()) / (24 * 3600))
